d1b7dbb2c47757aae6c4ca75a2d05f7773195fa4
Jay Welsh <thealchemicalopus@gmail.com>
2023-03-04 14:56:55 +0200

    feat(migrations): seed networks

--- src/database/migrations/20221207164846_network.js
+++ src/database/migrations/20221207164846_network.js
@@ -4,6 +4,21 @@ exports.up = (knex) => knex.schema.createTable(NETWORK_TABLE, table => {
     table.increments();
     table.string("name").index().unique().notNullable();
     table.timestamps(true, true);
+}).then(function () {
+    return knex(NETWORK_TABLE).insert([
+      {
+        name: "ethereum",
+      },
+      {
+        name: "arbitrum",
+      },
+      {
+        name: "optimism",
+      },
+      {
+        name: "canto",
+      },
+    ]);
 });
 
 exports.down = knex => knex.schema.dropTable(NETWORK_TABLE);
