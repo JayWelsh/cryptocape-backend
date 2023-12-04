16ccf0874f52b2d7a264778b3b73187556a6c2bd
Jay Welsh <thealchemicalopus@gmail.com>
2023-12-05 00:51:59 +0200

    chore(snapshot-resolutions): enable optimisation pass 2

--- src/tasks/get-combined-account-value-snapshot-history-of-accounts.ts
+++ src/tasks/get-combined-account-value-snapshot-history-of-accounts.ts
@@ -41,20 +41,29 @@ export const getCombinedValueBreakdownTimeseries = async (addressesArray: string
 
   let accountValueSnapshotsHourly = await AccountValueSnapshotHourlyRepository.getSnapshotHistoryByAddresses(addressesArray);
 
+  let currentTimestamp = new Date().getTime();
+  let maxHourlyHistory = (60 * 60 * 24 * 30) * 1000 // 30 days
+  let lowResInterval = (60 * 60 * 12) * 1000;
+
   for(let accountValueSnapshot of accountValueSnapshotsHourly) {
     let {
       value_usd,
       timestamp,
     } = accountValueSnapshot;
     if(minutelyTimestamps.indexOf(timestamp.getTime()) === -1) {
-      if(new BigNumber(value_usd).isGreaterThan(1)) {
-        if(timestampToTimeseries[timestamp]) {
-          timestampToTimeseries[timestamp].value = new BigNumber(timestampToTimeseries[timestamp].value).plus(value_usd).toString();
-        } else {
-          timestampToTimeseries[timestamp] = {
-            value: new BigNumber(value_usd).toString(),
-            timestamp,
-          };
+      if(
+        ((currentTimestamp - timestamp.getTime()) <= maxHourlyHistory) 
+        || (timestamp.getTime() % lowResInterval === 0)
+      ) { // 12 hourly data after 30 days as per maxHourlyHistory
+        if(new BigNumber(value_usd).isGreaterThan(1)) {
+          if(timestampToTimeseries[timestamp]) {
+            timestampToTimeseries[timestamp].value = new BigNumber(timestampToTimeseries[timestamp].value).plus(value_usd).toString();
+          } else {
+            timestampToTimeseries[timestamp] = {
+              value: new BigNumber(value_usd).toString(),
+              timestamp,
+            };
+          }
         }
       }
     }
