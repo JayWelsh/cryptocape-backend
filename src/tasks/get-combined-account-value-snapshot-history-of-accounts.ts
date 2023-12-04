ddac315356f46db0ef30d430aaac356eb4c4ff1b
Jay Welsh <thealchemicalopus@gmail.com>
2023-12-04 23:20:38 +0200

    chore(snapshot-resolutions): optimisation

--- src/tasks/get-combined-account-value-snapshot-history-of-accounts.ts
+++ src/tasks/get-combined-account-value-snapshot-history-of-accounts.ts
@@ -39,26 +39,26 @@ export const getCombinedValueBreakdownTimeseries = async (addressesArray: string
     }
   }
 
-  let accountValueSnapshotsHourly = await AccountValueSnapshotHourlyRepository.getSnapshotHistoryByAddresses(addressesArray);
+  // let accountValueSnapshotsHourly = await AccountValueSnapshotHourlyRepository.getSnapshotHistoryByAddresses(addressesArray);
 
-  for(let accountValueSnapshot of accountValueSnapshotsHourly) {
-    let {
-      value_usd,
-      timestamp,
-    } = accountValueSnapshot;
-    if(minutelyTimestamps.indexOf(timestamp) === -1) {
-      if(new BigNumber(value_usd).isGreaterThan(1)) {
-        if(timestampToTimeseries[timestamp]) {
-          timestampToTimeseries[timestamp].value = new BigNumber(timestampToTimeseries[timestamp].value).plus(value_usd).toString();
-        } else {
-          timestampToTimeseries[timestamp] = {
-            value: new BigNumber(value_usd).toString(),
-            timestamp,
-          };
-        }
-      }
-    }
-  }
+  // for(let accountValueSnapshot of accountValueSnapshotsHourly) {
+  //   let {
+  //     value_usd,
+  //     timestamp,
+  //   } = accountValueSnapshot;
+  //   if(minutelyTimestamps.indexOf(timestamp) === -1) {
+  //     if(new BigNumber(value_usd).isGreaterThan(1)) {
+  //       if(timestampToTimeseries[timestamp]) {
+  //         timestampToTimeseries[timestamp].value = new BigNumber(timestampToTimeseries[timestamp].value).plus(value_usd).toString();
+  //       } else {
+  //         timestampToTimeseries[timestamp] = {
+  //           value: new BigNumber(value_usd).toString(),
+  //           timestamp,
+  //         };
+  //       }
+  //     }
+  //   }
+  // }
 
   for (let [timestamp, timestampToTimeseriesEntry] of Object.entries(timestampToTimeseries)) {
     timeseries.push(timestampToTimeseriesEntry);
